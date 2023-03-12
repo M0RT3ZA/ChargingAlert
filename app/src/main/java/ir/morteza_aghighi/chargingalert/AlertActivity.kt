32f06838d0c18a8f8a8f98ca02333222c3bdf723
Morteza Aghighi <m0rt3z4.a@gmail.com>
2023-03-12 14:01:51 +0330

    New Code layout for AlertActivity

--- app/src/main/java/ir/morteza_aghighi/chargingalert/AlertActivity.kt
+++ app/src/main/java/ir/morteza_aghighi/chargingalert/AlertActivity.kt
@@ -1,8 +1,11 @@
 package ir.morteza_aghighi.chargingalert
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.RingtoneManager
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.provider.Settings
@@ -14,86 +17,108 @@ import ir.morteza_aghighi.chargingalert.model.BatteryInfoModel
 import ir.morteza_aghighi.chargingalert.tools.SharedPrefs
 
 class AlertActivity : AppCompatActivity() {
+    private lateinit var dismissMessage: String
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var cancelTimer: CountDownTimer
-    private lateinit var activityAlertBinding: ActivityAlertBinding
     private lateinit var audioManager: AudioManager
+    private var bypassDND = false
+    private var isUnplugged = false
+    private var isDischargeAlert = false
+    private var isDNDoff = false
     private var currentVolume = 0
-    private var isDNDoff = (Settings.Global.getInt(contentResolver, "zen_mode") == 0)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityAlertBinding = ActivityAlertBinding.inflate(layoutInflater)
-        setContentView(activityAlertBinding.root)
-        window.addFlags(
-            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                    or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
-                    or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
-        )
-        val dismissMessage = if (intent.getBooleanExtra("alertType", false)){
+        initVariables()
+    }
+
+    private fun initVariables() {
+        isDischargeAlert = intent.getBooleanExtra("alertType", false)
+        isDNDoff = Settings.Global.getInt(contentResolver, "zen_mode") == 0
+        dismissMessage = if (isDischargeAlert) {
             getString(R.string.disChargeDismissMessage)
-        }else getString(R.string.chargeDismissMessage)
+        } else getString(R.string.chargeDismissMessage)
+        bypassDND = SharedPrefs.getBoolean("bypassDND", applicationContext)
+        isUnplugged = BatteryInfoModel().getBatChargingType() == "Unplugged"
+        isDischargeAlert = intent.getBooleanExtra("alertType", false)
+        audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+        currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        mediaPlayer = MediaPlayer.create(
+            applicationContext, Settings.System.DEFAULT_ALARM_ALERT_URI
+        )
+        initUI()
+    }
+
+    private fun initUI() {
+
+        val activityAlertBinding: ActivityAlertBinding =
+            ActivityAlertBinding.inflate(layoutInflater)
+        setContentView(activityAlertBinding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            setTurnScreenOn(true)
+            val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            keyguardManager.requestDismissKeyguard(this, null)
+            setShowWhenLocked(true)
+        } else {
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                        or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                        or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+            )
+        }
+
         activityAlertBinding.btnSnooze.setOnClickListener {
             Toast.makeText(
-                this@AlertActivity,
-                dismissMessage,
-                Toast.LENGTH_LONG
+                this@AlertActivity, dismissMessage, Toast.LENGTH_LONG
             ).show()
             finish()
         }
-        try {
-            if (isDNDoff || SharedPrefs.getBoolean("bypassDND", applicationContext)){
-                audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
-                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-                mediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_ALARM_ALERT_URI)
-                currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-                mediaPlayer = MediaPlayer.create(
-                    applicationContext,
-                    Settings.System.DEFAULT_ALARM_ALERT_URI
-                )
-                audioManager.setStreamVolume(
-                    AudioManager.STREAM_MUSIC,
-                    audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
-                    0
-                )
-                mediaPlayer.setVolume(
-                    SharedPrefs.getInt("volume", applicationContext)
-                        .toFloat() / (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * 10),
-                    SharedPrefs.getInt("volume", applicationContext)
-                        .toFloat() / (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * 10)
-                )
-                mediaPlayer.start()
-            }
-            cancelTimer = object : CountDownTimer(60000, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    if (BatteryInfoModel().getBatChargingType() == "Unplugged"
-                        && !intent.getBooleanExtra("alertType",false)) {
-                        cancelTimer.cancel()
-                        finish()
-                    }
-                }
+        initFunctions()
+    }
 
-                override fun onFinish() {
-                    Toast.makeText(
-                        this@AlertActivity,
-                        dismissMessage,
-                        Toast.LENGTH_LONG
-                    ).show()
-                    if (isDNDoff || SharedPrefs.getBoolean("bypassDND", applicationContext)){
-                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0)
-                        mediaPlayer.release()
-                    }
+    private fun initFunctions() {
+
+        if ((isDNDoff || bypassDND) && ((isDischargeAlert && isUnplugged) || (!isDischargeAlert && !isUnplugged))) {
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+            audioManager.setStreamVolume(
+                AudioManager.STREAM_MUSIC,
+                audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                0
+            )
+            mediaPlayer.setVolume(
+                SharedPrefs.getInt("volume", applicationContext)
+                    .toFloat() / (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * 10),
+                SharedPrefs.getInt("volume", applicationContext)
+                    .toFloat() / (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * 10)
+            )
+            mediaPlayer.start()
+        }
+        cancelTimer = object : CountDownTimer(60000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                if (isUnplugged && !isDischargeAlert) {
+                    cancelTimer.cancel()
                     finish()
                 }
-            }.start()
-        } catch (ignored: Exception) {
-        }
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@AlertActivity, dismissMessage, Toast.LENGTH_LONG
+                ).show()
+                if (isDNDoff || bypassDND) {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0)
+                    mediaPlayer.release()
+                }
+                finish()
+            }
+        }.start()
     }
 
     override fun onDestroy() {
         try {
             cancelTimer.cancel()
-            if (isDNDoff || SharedPrefs.getBoolean("bypassDND", applicationContext)){
+            if (isDNDoff || bypassDND) {
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0)
                 mediaPlayer.release()
             }
