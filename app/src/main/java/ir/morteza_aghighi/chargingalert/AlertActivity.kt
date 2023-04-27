417ef099949c2af096ea8a875952ecd32dd3227e
Morteza on Lenovo-11thGen <developer.morteza.92@gmail.com>
2023-04-28 01:10:10 +0330

    finish comments of AlertActivity.

--- app/src/main/java/ir/morteza_aghighi/chargingalert/AlertActivity.kt
+++ app/src/main/java/ir/morteza_aghighi/chargingalert/AlertActivity.kt
@@ -17,6 +17,8 @@ import ir.morteza_aghighi.chargingalert.model.BatteryInfoModel
 import ir.morteza_aghighi.chargingalert.tools.SharedPrefs
 
 class AlertActivity : AppCompatActivity() {
+    /** declaring variables,
+     * the descriptions for each one is in the code.*/
     private lateinit var dismissMessage: String
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var chargeDischargeAlertCancelTimer: CountDownTimer
@@ -25,7 +27,7 @@ class AlertActivity : AppCompatActivity() {
     private var isUnplugged = false
     private var isDischargeAlert = false
     private var isDNDoff = false
-    private var currentVolume = 0
+    private var defaultVolume = 0
     private lateinit var batteryInfoModel: BatteryInfoModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,7 +74,7 @@ class AlertActivity : AppCompatActivity() {
          * setting up audio manager and media player to be able to set user preferred volume.
          * you will see the logic in the {@link #initFunctions() initFunctions} function*/
         audioManager = getSystemService(AUDIO_SERVICE) as AudioManager
-        currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        defaultVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
         mediaPlayer = MediaPlayer.create(
             applicationContext, Settings.System.DEFAULT_ALARM_ALERT_URI
         )
@@ -112,31 +114,61 @@ class AlertActivity : AppCompatActivity() {
             ).show()
             finish()
         }
+
         initFunctions()
     }
 
+    /**
+     * this function sets up alarm sound and mechanism to check if user connects/disconnects charger
+     * and if user connects charger on low battery or disconnects charger when upper charge limit reached
+     * this function stops alert and closes activity*/
     private fun initFunctions() {
 
-        if ((isDNDoff || bypassDND) && ((isDischargeAlert && isUnplugged) || (!isDischargeAlert && !isUnplugged))) {
+        /** check if DND is off or if it is on the bypass DND option is enabled by the user. */
+        if ((isDNDoff || bypassDND)
+
+
+            /** checking 2 different condition and one of them must return true.
+             * first check if the alert type is for discharge and the battery is still unplugged.
+             * second one checks if the alert type is for charge and charger is still plugged in.*/
+            && ((isDischargeAlert && isUnplugged) || (!isDischargeAlert && !isUnplugged))) {
+
+            /** if the above conditions are met first getting the default alarm music for the phone.*/
             RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+
+            /** then temporary increase the volume of music playback to max level because the amount of volume that user set for alarm is
+             * according to the max volume. if we do not increase the playback volume, the alarm volume is probably lower than
+             * what user wanted.*/
             audioManager.setStreamVolume(
                 AudioManager.STREAM_MUSIC,
                 audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
                 0
             )
+
+            /** setting user preferred volume for both channels*/
             mediaPlayer.setVolume(
                 SharedPrefs.getInt("volume", applicationContext)
                     .toFloat() / (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * 10),
                 SharedPrefs.getInt("volume", applicationContext)
                     .toFloat() / (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * 10)
             )
+
+            /** playing alarm*/
             mediaPlayer.start()
         }
+
+        /** the bellow code is a timer that has 2 jobs:*/
         chargeDischargeAlertCancelTimer = object : CountDownTimer(60000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
+
+                /** first: checks every second for if on discharge alert user connected charger
+                 * of on charging alert user is disconnected from charger.
+                 * and if conditions are met stops alert, countdown timer and finishes activity,
+                 * and restore default device volume.*/
                 isUnplugged = batteryInfoModel.getBatChargingType() == "Unplugged"
 
                 if ((isUnplugged && !isDischargeAlert) || (!isUnplugged && isDischargeAlert)) {
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, defaultVolume, 0)
                     chargeDischargeAlertCancelTimer.cancel()
                     finish()
                 }
@@ -144,11 +176,15 @@ class AlertActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                /** second: if the talked conditions were not met for 60 seconds stops alarm,
+                 * shows a message that the alert will repeat if you the user do not connect/disconnect charger
+                 * in next 5 minutes, and finishes this activity,
+                 * and restore default device volume.*/
                 Toast.makeText(
                     this@AlertActivity, dismissMessage, Toast.LENGTH_LONG
                 ).show()
                 if (isDNDoff || bypassDND) {
-                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0)
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, defaultVolume, 0)
                     mediaPlayer.release()
                 }
                 finish()
@@ -158,9 +194,10 @@ class AlertActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         try {
+            /** onDestroy cancel countdown timer and alarm, and restore default device volume.*/
             chargeDischargeAlertCancelTimer.cancel()
             if (isDNDoff || bypassDND) {
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, currentVolume, 0)
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, defaultVolume, 0)
                 mediaPlayer.release()
             }
         } catch (ignored: Exception) {
